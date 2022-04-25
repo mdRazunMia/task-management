1c5fd36e904e053f7cb770ca94e184fc9f54271e
Md. Razun Mia <razun.neub.cse@gmail.com>
2022-04-25 15:29:48 +0600

    Implement modified group create model and function for creating group.

--- models/groupModel.js
+++ models/groupModel.js
@@ -1,3 +1,60 @@
+// const mongoose = require("mongoose");
+
+// const GroupSchema = mongoose.Schema(
+//   {
+//     group_title: {
+//       type: String,
+//       required: [true, "Group name is required."],
+//       trim: true,
+//     },
+//     group_task_list: [
+//       {
+//         task_id: {
+//           type: String,
+//           default: null,
+//         },
+//         task_title: {
+//           type: String,
+//           default: null,
+//         },
+//       },
+//     ],
+//     nested: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     super_group: {
+//       super_group_id: {
+//         type: String,
+//         default: null,
+//       },
+//       super_group_title: {
+//         type: String,
+//         trim: true,
+//         default: null,
+//       },
+//       super_group_task_list: [
+//         {
+//           task_id: {
+//             type: String,
+//           },
+//           task_title: {
+//             type: String,
+//           },
+//         },
+//       ],
+//     },
+//   },
+//   {
+//     timestamps: true,
+//     versionKey: false,
+//   }
+// );
+
+// module.exports = mongoose.model("Group", GroupSchema);
+
+// modified model
+
 const mongoose = require("mongoose");
 
 const GroupSchema = mongoose.Schema(
@@ -23,27 +80,25 @@ const GroupSchema = mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    super_group: {
-      super_group_id: {
-        type: String,
-        default: null,
-      },
-      super_group_title: {
-        type: String,
-        trim: true,
-        default: null,
-      },
-      super_group_task_list: [
-        {
-          task_id: {
-            type: String,
-          },
-          task_title: {
-            type: String,
-          },
+    sub_group: [
+      {
+        sub_group_title: {
+          type: String,
+          trim: true,
+          default: null,
         },
-      ],
-    },
+        sub_group_task_list: [
+          {
+            task_id: {
+              type: String,
+            },
+            task_title: {
+              type: String,
+            },
+          },
+        ],
+      },
+    ],
   },
   {
     timestamps: true,
